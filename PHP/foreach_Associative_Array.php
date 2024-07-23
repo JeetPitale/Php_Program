388f8e81839b1cdce7061b933af992e40fea66b6
Jeet Pitale <jeetdipakpitale1107@gmail.com>
2024-07-23 11:14:28 +0530

    Delete PHP directory
    Signed-off-by: Jeet Pitale <jeetdipakpitale1107@gmail.com>

--- PHP/foreach_Associative_Array.php
+++
@@ -1,9 +0,0 @@
-<?php
-$birds=array("Peacock"=>"1","Pigeon"=>"2","Sparrow"=>"3","Humming Bird"=>"4");//This Data is Called Key and number is called Value
-
-foreach($birds as $x=>$y)
-{
-    echo $x." ".$y;
-    echo "<br>";
-}
-?>
--- PHP/foreach_Indexed_Array.php
+++
@@ -1,9 +0,0 @@
-<?php
-$birds=array("Peacock","Pigeon","Sparrow","Humming Bird");//This Data is Called Value
-
-foreach($birds as $x)
-{
-    echo "I like " .$x;
-    echo "<br>";
-}
-?>
